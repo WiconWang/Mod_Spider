dc9021ac218ba6fd0b8915235ca1fd9fff98f84c
wicon wang <wiconwang@gmail.com>
2018-05-18 13:36:23 +0800

    写错字母

--- TakePhotosAndUpload/start.sh
+++ TakePhotosAndUpload/start.sh
@@ -7,5 +7,5 @@ fswebcam --no-banner -r 640x480 $path"1.jpg"
 #USB摄像头第一张一般都不清晰，所以一秒后再拍一张
 sleep 1
 fswebcam --no-banner -r 640x480 $path".jpg"
-python3 processer.py $path+".jpg"
+python3 processer.py $path".jpg"
 rm -f $path"1.jpg"
