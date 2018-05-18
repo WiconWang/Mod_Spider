97f8048f021ce36d34bfa7a8642337e2bf7059cb
wicon wang <wiconwang@gmail.com>
2018-05-18 12:43:07 +0800

    Improved: USB摄像头第一张一般都不清晰，所以一秒后再拍一张

--- TakePhotosAndUpload/start.sh
+++ TakePhotosAndUpload/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 nowtime=`date +%Y%m%d_%H%M%S`
-path="/home/pi/Monitor/Cam_$nowtime.jpg"
-fswebcam --no-banner -r 640x480 $path
-python3 processer.py  $path
+path="/home/pi/Monitor/Cam_$nowtime"
+fswebcam --no-banner -r 640x480 $path+"1.jpg"
+#USB摄像头第一张一般都不清晰，所以一秒后再拍一张
+sleep 1
+fswebcam --no-banner -r 640x480 $path+"2.jpg"
+sleep 1
+fswebcam --no-banner -r 640x480 $path+".jpg"
+python3 processer.py  $path+".jpg"
+#rm -f $path+"1.jpg"
