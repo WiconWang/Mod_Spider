7b2f0c9a2d88d6980ae671abd94ec0eed4230d8c
wicon wang <wiconwang@gmail.com>
2018-05-20 18:15:01 +0800

    重新添加图片算法，使用灰度

--- TakePhotosAndUpload/monitor.py
+++ TakePhotosAndUpload/monitor.py
@@ -19,15 +19,86 @@ from config import (
 
 
 class Diff:
-    def pics(self, file1, file2):
-        # 把图像对象转换为直方图数据，存在list h1、h2 中
-        h1 = Image.open(file1).histogram()
-        h2 = Image.open(file2).histogram()
-        return math.sqrt(reduce(operator.add, list(
-            map(lambda a, b: (a - b) ** 2, h1, h2))) / len(h1))
-        # sqrt:计算平方根，reduce函数：前一次调用的结果和sequence的下一个元素传递给operator.add
-        # operator.add(x,y)对应表达式：x+y
-        # 这个函数是方差的数学公式：S^2= ∑(X-Y) ^2 / (n-1)
+
+    def calculate(self, image1, image2):
+        g = image1.histogram()
+        s = image2.histogram()
+        assert len(g) == len(s), "error"
+
+        data = []
+
+        for index in range(0, len(g)):
+            if g[index] != s[index]:
+                data.append(1 - abs(g[index] - s[index]) / max(g[index], s[index]))
+            else:
+                data.append(1)
+
+        return sum(data) / len(g)
+
+    def split_image(self, image, part_size):
+        pw, ph = part_size
+        w, h = image.size
+
+        sub_image_list = []
+
+        assert w % pw == h % ph == 0, "error"
+
+        for i in range(0, w, pw):
+            for j in range(0, h, ph):
+                sub_image = image.crop((i, j, i + pw, j + ph)).copy()
+                sub_image_list.append(sub_image)
+
+        return sub_image_list
+
+    def classfiy_histogram_with_split(self, image1, image2, size=(640, 480), part_size=(32, 24)):
+        '''
+         'image1' 和 'image2' 都是Image 对象.
+         可以通过'Image.open(path)'进行创建。
+         'size' 重新将 image 对象的尺寸进行重置，默认大小为256 * 256 .
+         'part_size' 定义了分割图片的大小.默认大小为64*64 .
+         返回值是 'image1' 和 'image2'对比后的相似度，相似度越高，图片越接近，达到100.0说明图片完全相同。
+        '''
+        img1 = image1.resize(size).convert("L")
+        sub_image1 = self.split_image(img1, part_size)
+
+        img2 = image2.resize(size).convert("L")
+        sub_image2 = self.split_image(img2, part_size)
+
+        sub_data = 0
+        for im1, im2 in zip(sub_image1, sub_image2):
+            sub_data += self.calculate(im1, im2)
+
+        x = size[0] / part_size[0]
+        y = size[1] / part_size[1]
+
+        pre = round((sub_data / (x * y)), 6)
+        return pre * 100
+
+    # def getImgHash(self, fne):
+    #     image_file = Image.open(fne)  # 打开
+    #     image_file = image_file.resize((12, 12))  # 重置图片大小我12px X 12px
+    #     image_file = image_file.convert("L")  # 转256灰度图
+    #     Grayls = getGray(image_file)  # 灰度集合
+    #     avg = getAvg(Grayls)  # 灰度平均值
+    #     bitls = ''  # 接收获取0或1
+    #     # 除去变宽1px遍历像素
+    #     for h in range(1, image_file.size[1] - 1):  # h
+    #         for w in range(1, image_file.size[0] - 1):  # w
+    #             if image_file.getpixel((w, h)) >= avg:  # 像素的值比较平均值 大于记为1 小于记为0
+    #                 bitls = bitls + '1'
+    #             else:
+    #                 bitls = bitls + '0'
+    #     return bitls
+    #
+    # def pics(self, file1, file2):
+    #     # 把图像对象转换为直方图数据，存在list h1、h2 中
+    #     h1 = Image.open(file1).histogram()
+    #     h2 = Image.open(file2).histogram()
+    #     return math.sqrt(reduce(operator.add, list(
+    #         map(lambda a, b: (a - b) ** 2, h1, h2))) / len(h1))
+    #     # sqrt:计算平方根，reduce函数：前一次调用的结果和sequence的下一个元素传递给operator.add
+    #     # operator.add(x,y)对应表达式：x+y
+    #     # 这个函数是方差的数学公式：S^2= ∑(X-Y) ^2 / (n-1)
 
 
 class UploadMain:
@@ -91,7 +162,7 @@ class SendMessage:
         pass
 
     def weixin(self, url):
-        return [0, 'WXerror'+ url]
+        return [0, 'WXerror' + url]
 
 
 if __name__ == '__main__':
@@ -99,21 +170,23 @@ if __name__ == '__main__':
     # if sys.argv[1]:
     #     process(sys.argv[1])
     # pass
-    res = 0
+    res = 100
     # 鉴定两个图差别
     if sys.argv[1] and sys.argv[2]:
         diffClass = Diff()
-        res = diffClass.pics(sys.argv[1], sys.argv[2])
+        res = diffClass.classfiy_histogram_with_split(Image.open(sys.argv[1]), Image.open(sys.argv[2]))
 
-    if res > 3000:
+    # 相似度低于75时，上传图片
+    # print(sys.argv[1] + " + " + sys.argv[2] + "  : " + str(res))
+    if res < 75:
         # 如果差别过大，则上传1号图片图片
         uploadClass = UploadMain()
         urls = uploadClass.process(sys.argv[1])
         # 上传步骤无错误时，进行去发送微信通知
         if urls[0] == 0:
-            messageClass = SendMessage()
-            msg = messageClass.weixin('http://'+urls[1])
-            print('成功')
-            print(msg)
+            # messageClass = SendMessage()
+            # msg = messageClass.weixin('http://' + urls[1])
+            print("WARING" + sys.argv[1] + " + " + sys.argv[2] + "  : " + str(res))
         else:
-            print(urls[1])
+            print(sys.argv[1] + " + " + sys.argv[2] + "  : " + str(res))
+
--- TakePhotosAndUpload/start.sh
+++ TakePhotosAndUpload/start.sh
@@ -10,19 +10,24 @@
 # python3 processer.py $path".jpg"
 # rm -f $path"1.jpg"
 
- cd `dirname $0`
- folder="/home/pi/Monitor"
- # 取上次的图片
- last_file=`ls -t $folder/*.jpg | head -1`
+cd `dirname $0`
+folder="/home/pi/Monitor"
+# 取上次的图片
+last_file=`ls -t $folder/*.jpg | head -1`
 
- # 开始拍图片
- nowtime=`date +%Y%m%d_%H%M%S`
- path=$folder"/Cam_$nowtime"
- fswebcam --no-banner -r 640x480 $path"1.jpg"
- #USB摄像头第一张一般都不清晰，所以一秒后再拍一张
- rm -f $path"1.jpg"
- sleep 1
- fswebcam --no-banner -r 640x480 $path".jpg"
+# 开始拍图片
+nowtime=`date +%Y%m%d_%H%M%S`
+path=$folder"/Cam_$nowtime"
 
- python3 ./monitor.py $path".jpg" $last_file
+#USB摄像头第一张一般都不清晰，所以一秒后再拍一张
+fswebcam --no-banner -r 640x480 $path"1.jpg"
+rm -f $path"1.jpg"
+sleep 1
+fswebcam --no-banner -r 640x480 $path"2.jpg"
+rm -f $path"2.jpg"
+sleep 1
+
+fswebcam --no-banner -r 640x480 $path".jpg"
+
+python3 ./monitor.py $path".jpg" $last_file
 
