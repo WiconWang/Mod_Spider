52e9496b068e59bc9a7163be89dc3a2caeda134b
wicon wang <wiconwang@gmail.com>
2018-07-10 11:15:05 +0800

    对于win系统进行优化，添加对应版本的ffmpeg主程序

--- Video/SpeedAjustFfmpeg.py
+++ Video/SpeedAjustFfmpeg.py
@@ -3,12 +3,14 @@ import time
 import os
 import subprocess
 import random
+import platform
 
 
 def substring(date):
     r = date.decode()
-    r = r.strip()
-    rlist = r.split(":")
+    r = r.replace('Duration:', '').strip()
+    r1 = r.split(",")
+    rlist = r1[0].split(":")
     result = (int(rlist[0]) * 60 * 60) + (int(rlist[1]) * 60) + (float(rlist[2]))
     return result
 
@@ -23,12 +25,18 @@ def division(path, path2):
         file_temp = time.strftime('_%Y%m%d_%H%M%S_', time.localtime())
         file_temp_step1 = file_temp + "step1_"
 
-        # 获取当前文件的视频长度
-        strcmd = ["ffmpeg -y -i " + path + file + " 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//"]
+        # 检测终端类型并选择不同命令
+        cmd_grep = ("find" if ('Windows' in platform.system()) else "grep")
+        strcmd = 'ffmpeg -y -i ' + path + file + ' 2>&1 | ' + cmd_grep + ' "Duration"'
+
         result = subprocess.run(args=strcmd, stdout=subprocess.PIPE, shell=True)
         date = result.stdout
+        # 解析出具体的时间
         video_time = substring(date)
 
+        if not video_time or video_time == 0:
+            print("无法解析此文件时间")
+            break
         # print(type(date))
         # print(date)
         # print(file_temp_step1)
@@ -59,7 +67,7 @@ def division(path, path2):
             file_step1 = path2 + file_temp + "step1_" + str(count) + '.mp4'
             file_step2 = path2 + file_temp + "step2_" + str(count) + '.mp4'
             # 本碎片的速度
-            pts = random.randint(50, 200) / 100
+            pts = random.randint(95, 105) / 100
             if pts == 1:
                 pts = 1.1
             # 声音和视频正好相反，因此取反
@@ -69,26 +77,28 @@ def division(path, path2):
                 pts_audio) + '[a]" -map "[v]" -map "[a]" ' + file_step2 + '  -loglevel quiet'
             video_result = subprocess.run(args=sub, shell=True)
             # print(video_result)
-            print("- 文件碎片" + str(count) + "号的速度，已调整为" + str(pts) + "倍")
+            print("- 文件碎片 " + str(count) + "号的速度，已调整为" + str(pts) + "倍")
 
             start = end
             count = count + 1
 
         time.sleep(2)
         merge(path2, file, file_temp, count - 1)
+    print("---")
+    input("任务已完成，请关闭窗口")
 
 
 # 传入 生成目标位置，文件名， 临时文件名，临时文件最大号，原文件fps，原文件size
 def merge(path, file, file_temp, count):
     print(count)
-    all_file = ''
+    # all_file = ''
     sub = 'ffmpeg  -y'
-    filter_complex = ' -filter_complex \''
+    filter_complex = ' -filter_complex "'
     for i in range(1, count + 1):
         sub += ' -i  process/' + file_temp + "step2_" + str(i) + '.mp4 '
         filter_complex += ' [' + str(i - 1) + ':0] [' + str(i - 1) + ':1] '
-    sub += " -r 25 " + filter_complex + "concat=n=" + str(
-        count) + ":v=1:a=1 [v] [a]' -map '[v]' -map '[a]' " + path + file + '  -loglevel quiet'
+    sub += ' -r 25 ' + filter_complex + 'concat=n=' + str(
+        count) + ':v=1:a=1 [v] [a]" -map "[v]" -map "[a]" ' + path + file + '  -loglevel quiet'
 
     # ffmpeg -i process/_20180709_000404_step2_1.mp4
     # -i process/_20180709_000404_step2_2.mp4
@@ -96,28 +106,30 @@ def merge(path, file, file_temp, count):
     # -i process/_20180709_000404_step1_1.mp4
     # -filter_complex '[0:0] [0:1] [1:0] [1:1] [2:0] [2:1] [3:0] [3:1] concat=n=4:v=1:a=1 [v] [a]' -map '[v]' -map '[a]' output.mp4
     # print(sub)
-    print("+ 正在合并文件碎片" + file + " ")
+    print("+ 正在合并文件碎片 " + file + " ")
     video_result = subprocess.run(args=sub, shell=True)
 
-    # for i in range(1, count + 1):
-    #     os.remove(path + file_temp + "step1_" + str(i) + '.mp4')
-    #     os.remove(path + file_temp + "step2_" + str(i) + '.mp4')
+    time.sleep(2)
+    for i in range(1, count + 1):
+        os.remove(path + file_temp + "step1_" + str(i) + '.mp4')
+        os.remove(path + file_temp + "step2_" + str(i) + '.mp4')
     print("* 工作完成：" + file + "，请到 process 文件夹下 拷走 此文件！")
+    print("---")
 
 
 if __name__ == '__main__':
     try:
         # 硬盘路径(原视频存放路径)
-        path = os.path.dirname(os.path.realpath(__file__)) + "/"
+        path = os.path.dirname(os.path.realpath(__file__)) + "/source"
         # 切割后的视频存放路径
-        path2 = path + "process"
+        path2 = os.path.dirname(os.path.realpath(__file__)) + "/process"
         if not os.path.exists(path2):
             os.mkdir(path2, 0o777)
         if not os.path.exists(path2):
             print("警告，程序出错。无法新建process文件夹，请手动建立")
         else:
-            path2 = path2 + "/"
-            division(path, path2)
+            division(path + "/", path2 + "/")
     except Exception as e:
         print("警告，程序出错:")
         print(e)
+
