2fe17dbb48e721b00a1cb617c310497bc642cdf8
wicon wang <wiconwang@gmail.com>
2018-09-06 08:17:00 +0800

    调整

--- Crontab_RPi/read_ximalaya_fund.py
+++ Crontab_RPi/read_ximalaya_fund.py
@@ -9,6 +9,10 @@ import redis
 class Task(object):
     def __init__(self):
         self.redis = redis.Redis('localhost', 6379)
+        self.header = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/63.0.3239.132 Safari/537.36'}
+
 
     def main(self):
         # 选择一系列地址，并查询最新一第是否已经在缓存 中有记录，如果有记录则找下一地址，直到找到一个最新的
@@ -17,26 +21,21 @@ class Task(object):
             'https://www.ximalaya.com/revision/album/getTracksList?albumId=9444470&pageNum=1',
         ]
         for i in url:
-            audio_url = self.getjson(i)
+            paths = self.getjson(i)
+            audio_url = paths[0]
             if audio_url:
                 res_r = self.redis.sismember("audio_xmly_url", audio_url)
                 if not res_r:
                     break
         # print(audio_url)
-        # local_url = self.download(audio_url)
+        local_url = self.download(audio_url)
         # print(local_url)
         # 直接播放语音
-        self.redis.sadd("audio_xmly_url", audio_url)
-        # os.system('mplayer  -af volume=+15  "%s"' % local_url)
-        os.system('mplayer  -af volume=+15  "%s"' % audio_url)
+        os.system('mplayer  -af volume=+15  "%s"' % local_url)
 
     def getjson(self, url):
-        header = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/63.0.3239.132 Safari/537.36'}
-
         # 请求远端服务器并选取JSON
-        r = requests.get(url=url, headers=header)
+        r = requests.get(url=url, headers=self.header)
         if r.status_code != 200:
             print('404 Error')
             return False
@@ -46,27 +45,28 @@ class Task(object):
 
         # 取详细情况
         url2 = "http://www.ximalaya.com/tracks/%d.json" % trackId
-        r = requests.get(url=url2, headers=header)
+        r = requests.get(url=url2, headers=self.header)
         if r.status_code != 200:
             print('404 Error')
             return False
         if not r.json()['play_path']:
             return False
-        return r.json()['play_path'], r.json()['album_id'], r.json()['play_path']
+        return r.json()['play_path'], r.json()['album_id']
         # return "http://%s/%s" % ('od.qingting.fm', r.json()['play_path'])
 
     # 提取出音频URL启动下载
-    # def download(self, audio_url):
-    #     timenow = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
-    #
-    #     r2 = requests.get(audio_url)
-    #     local_url = "%s/XMLY_%s.m4a" % (os.path.abspath(
-    #         os.path.join(os.path.dirname(__file__), "../Music")), timenow)
-    #     with open(local_url, "wb") as code:
-    #         code.write(r2.content)
-    #
-    #     self.redis.sadd("audio_xmly_url", audio_url)
-    #     return local_url
+    def download(self, audio_url):
+        timenow = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        print(audio_url)
+        print(self.header)
+        r2 = requests.get(url=audio_url, headers=self.header)
+        local_url = "%s/XMLY_%s.m4a" % (os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "../Music")), timenow)
+        with open(local_url, "wb") as code:
+            code.write(r2.content)
+
+        self.redis.sadd("audio_xmly_url", audio_url)
+        return local_url
 
 
 if __name__ == '__main__':
