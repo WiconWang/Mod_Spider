10d2aabb12cd8b4786c5a1706ed990b0271a7e4d
wicon wang <wiconwang@gmail.com>
2018-10-07 12:18:39 +0800

    添加报时

--- Crontab_RPi/read_rand_music.py
+++ Crontab_RPi/read_rand_music.py
@@ -5,8 +5,14 @@ import random
 import time
 
 
-def main(mp3path):
-    print(mp3path)
+def main(mp3path, readText):
+    text = "现在 " + time.strftime('%H:%M', time.localtime(time.time())) + " " + readText
+    print(text)
+    url = 'http://tts.baidu.com/text2audio?idx=1&tex={0}&cuid=baidu_speech_' \
+          'demo&cod=2&lan=zh&ctp=1&pdt=1&spd=4&per=4&vol=10&pit=8'.format(text)
+    # 直接播放语音
+    os.system('mplayer "%s"' % url)
+    time.sleep(1)
     # 先播放一首音乐做闹钟
     os.system('mplayer  -af volume=+5 %s' % mp3path)
 
@@ -29,6 +35,6 @@ if __name__ == '__main__':
     config.readfp(open(os.path.join(os.path.dirname(__file__), "config.ini")))
     # 早晨和晚上分别放不同的文件夹内容
     if int(time.strftime('%H', time.localtime(time.time()))) < 12:
-        main(rand(config.get("RAND_MUSIC", "path")))
+        main(rand(config.get("RAND_MUSIC", "path")), "快点起床了！")
     else:
-        main(rand(config.get("RAND_MUSIC", "path_night")))
+        main(rand(config.get("RAND_MUSIC", "path_night")), "准备睡觉了！")
